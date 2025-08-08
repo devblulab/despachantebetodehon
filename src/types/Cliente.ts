e0cb6fb5b9b158a9b8cfbb411c25a2a7ee121b98
devblulab <116671864+devblulab@users.noreply.github.com>
2025-08-08 10:18:18 -0300

    Update Cliente.ts

--- src/types/Cliente.ts
+++ src/types/Cliente.ts
@@ -1,16 +1,19 @@
-export interface Cliente {
-  id?: string;
-  placa?: string;
-  renavam?: string;
-  proprietarioatual?: string;
-  marca_modelo?: string;
-  origem?: string;
-  municipio?: string;
-  fone_residencial?: string;
-  fone_comercial?: string;
-  fone_celular?: string;
-  usuario?: string;
-  statusCRM?: string;
-  funnelId?: string; // ✅ Adicione essa linha
 
+
+export interface Cliente {
+  placa: string;
+  renavam: string;
+  proprietarioatual: string;
+  marca_modelo: string;
+  origem: string;
+  municipio: string;
+  observacao?: string;
+  fone_residencial: string;
+  fone_comercial: string;
+  fone_celular: string;
+  usuario: string;
+  statusCRM?: string;
+  dataAtualizacao?: string;
+  id?: string;
+  funnelId?: string; // Added to fix compile error
 }
